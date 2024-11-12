6fa349419226052a14a009a364e0c503e54c5cfd
Xavier Glenn <xglenn@appdev.yearup.org>
2024-11-12 08:21:26 -0700

    Initial Commit!

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    //fixme
+    //help
+    //todo
+    //note
+    //bookmark
+    //comment
+
+    //note: this may just be for calling the function for homescreen. We shall see.
+
+}
