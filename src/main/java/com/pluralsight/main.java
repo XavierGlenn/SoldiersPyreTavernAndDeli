ca7a8f0403ed54937e187ce6fb6838c1cee690d9
Xavier Glenn <xglenn@appdev.yearup.org>
2024-11-14 15:42:55 -0700

    Scanner eater lines added. Now for the functional menu and the full scanner, and I think it's good to go for testing!

--- src/main/java/com/pluralsight/main.java
+++ src/main/java/com/pluralsight/main.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class main {
-    public static void main(String[] args) {MainMenu();}
+    public static void Menu(String[] args) {MainMenu();}
 
     private static final Scanner scanner = new Scanner(System.in);
     private static final ArrayList<String> receiptItems = new ArrayList<>();
@@ -16,19 +16,26 @@ public class main {
 
     public static void MainMenu() {
         while (true) {
-            System.out.println("\n Attention! Welcome to the Soldier's Pyre Tavern and Deli. Could you go for a good meal? I've got you soldier.");
-            System.out.println("1. New Sandwich Order");
+            System.out.println("˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖");
+            System.out.println("\uD83D\uDD25 \uD83D\uDCA5 Attention! Welcome to the Soldier's Pyre Tavern and Deli. Could you go for a good meal? I've got you soldier. \uD83D\uDCA5 \uD83D\uDD25");
+            System.out.println("1. New Order");
             System.out.println("0. Exit the Menu.");
+            System.out.println("˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖");
             System.out.print("Count on me. I can put you to rights. Enter your choice here: ");
+
             int choice = scanner.nextInt();
             scanner.nextLine(); // scanner eater line
 
             if (choice == 1) {
                 startOrder(); }
             else if (choice == 0) {
+                System.out.println("˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊");
                 System.out.println("Don't burn yourself. Be careful out there, soldier.");
+                System.out.println("˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊");
                 break; } else {
+                System.out.println("˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊");
                 System.out.println("Heads up! That choice was invalid. Let me help. Sending you back to the menu..."); }
+            System.out.println("˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊");
         }
     }
 
@@ -37,6 +44,7 @@ public class main {
         totalPrice = 0.0;
 
         while (true) {
+            System.out.println("˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊");
             System.out.println("Hey you! Tell me what you need to fuel up.");
             System.out.println("1. Add Sandwich");
             System.out.println("2. Add Drink");
@@ -44,8 +52,10 @@ public class main {
             System.out.println("4. Checkout");
             System.out.println("5. Cancel Order");
             System.out.print("Enter your choice: ");
+            System.out.println("˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊");
             int choice = scanner.nextInt();
             System.out.println("Let's cook with fire!");
+            System.out.println("˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊˚-˚₊‧⁺˖˖⁺‧₊");
             scanner.nextLine(); // scanner eater line
 
             switch (choice) {
@@ -60,7 +70,8 @@ public class main {
     }
 
     private static void addSandwich() {
-        //Size
+
+        //Size choice menu:
         System.out.println("Aye aye! You can count on me. What size sandwich would you like?");
         String[] sizeChoices = {"four", "eight", "twelve"};
         double[] sizePrices = {5.50, 7.00, 8.50};
@@ -69,10 +80,10 @@ public class main {
         System.out.println("3.) 12 inch sandwich |$" + sizePrices[2]);
         int choice = scanner.nextInt();
         double sandwichSizeChoice = sizePrices[choice -1];
-        //TODO add a scanner in here and the eater code at the end. And make a better menu. (int name = scanner.nextInt)
+        scanner.nextLine(); // scanner eater line
+        //TODO add a scanner in here. And make a better menu. (int name = scanner.nextInt)
 
-
-        //Bread
+        //Bread choice menu:
         System.out.println("Let's march! What flavor of bread would you like?");
         System.out.println("1.) White Bread");
         System.out.println("2.) Wheat Bread");
@@ -80,39 +91,44 @@ public class main {
         System.out.println("4.) Tortilla Wrap");
         String[] breadChoices = {"White", "Wheat", "Rye", "Wrap"};
         choice = scanner.nextInt();
-        //TODO add a scanner in here and the eater code at the end. And make a better menu.
+        scanner.nextLine(); // scanner eater line
+        //TODO add a scanner in here. And make a better menu.
 
-        //Meat
+        //Meat choice menu:
         System.out.println("Let's go! What meat would you like?");
         String[] meatChoices = {"Steak", "Ham", "Salami", "Roast Beef", "Chicken", "Bacon", "None"};
         double[] meatPrices = {1.00, 2.00, 3.00, 3.50, 2.50, 1.75, 0.00};
-        //choice = scanner.nextDouble();
-        //TODO add a scanner in here and the eater code at the end. And make a better menu.
+        scanner.nextLine(); // scanner eater line
+        //TODO add a scanner in here. And make a better menu.
 
-        //Cheese
+        //Cheese choice menu:
         System.out.println("Got it. Now what cheese would you like?");
         String[] cheeseOptions = {"American", "Provolone", "Cheddar", "Swiss", "None"};
         double[] cheesePrices = {0.75, 1.50, 2.25, 1.75, 0.00};
         choice = scanner.nextInt();
-        //TODO add a scanner in here and the eater code at the end. And make a better menu.
+        scanner.nextLine(); // scanner eater line
+        //TODO add a scanner in here. And make a better menu.
 
-        //Topping
+        //Topping choice menu:
         System.out.println("On it.What toppings are you hungry for? And surprise! All of these are free!");
         String[] toppingOptions = {"Lettuce", "Peppers", "Onions", "Tomatoes", "Jalapenos", "Cucumbers", "Guacamole", "Mushrooms", "None"};
         double[] toppingPrices = {0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00};
         choice = scanner.nextInt();
-        //TODO add a scanner in here and the eater code at the end. And make a better menu.
+        scanner.nextLine(); // scanner eater line
+        //TODO add a scanner in here. And make a better menu.
 
-        //Condiments
+        //Condiments choice menu:
         System.out.println("Hang on. Did you want any condiments on that?");
         String[] condimentOptions = {"Mayonnaise", "Mustard", "Ketchup", "Ranch", "Thousand Island", "Vinaigrette"};
         double[] condimentPrices = {0.00, 0.00, 0.00, 0.00, 0.00, 0.00};
-        //TODO add a scanner in here and the eater code at the end. And make a better menu.
+        scanner.nextLine(); // scanner eater line
+        //TODO add a scanner in here. And make a better menu.
 
-        //Side
+        //Side choice menu:
         System.out.println("Incoming! Now did you want anything else on the side? Don't worry, I won't leave you hanging. These are free too!");
         String[] sidesOptions = {"Au Jus, Special Sauce"};
         double[] sidesPrices = {0.00, 0.00};
+        scanner.nextLine(); // scanner eater line
         //TODO add a scanner in here and the eater code at the end. And make a better menu.
 
         // Choose size:
@@ -131,7 +147,9 @@ public class main {
         // Choose extra meat option:
         System.out.print("Hup hup! Would you like extra meat for any extra hunger? (yes/no): ");
         String extraMeat = chooseOption("Extra Meat: ", meatChoices, meatPrices);
-        boolean extraMeatOptionsLabel = scanner.nextLine().equalsIgnoreCase("yes"); //TODO add a scanner in here and the eater code at the end
+        boolean extraMeatOptionsLabel = scanner.nextLine().equalsIgnoreCase("yes");
+        scanner.nextLine(); // scanner eater line
+        // TODO add a scanner in here
 
         // Choose cheese:
         String cheese = chooseOption("Cheese: ", cheeseOptions, cheesePrices);
@@ -140,7 +158,9 @@ public class main {
         // Choose extra cheese option:
         System.out.print("Tactical! Would you like extra cheese for any extra hunger? (yes/no): ");
         String extraCheese = chooseOption("Meats: ", meatChoices, meatPrices);
-        boolean extraCheeseOptionsLabel= scanner.nextLine().equalsIgnoreCase("yes"); //TODO add a scanner in here and the eater code at the end
+        boolean extraCheeseOptionsLabel= scanner.nextLine().equalsIgnoreCase("yes");
+        scanner.nextLine(); // scanner eater line
+        //TODO add a scanner in here
 
         // Choose topping:
         String topping = chooseOption("Topping: ", toppingOptions, toppingPrices);
@@ -156,7 +176,9 @@ public class main {
 
         // Toasted option:
         System.out.print("Steady now... Would you like your sandwich toasted? (yes/no): ");
-        boolean toasted = scanner.nextLine().equalsIgnoreCase("yes"); //TODO add a scanner in here and the eater code at the end
+        boolean toasted = scanner.nextLine().equalsIgnoreCase("yes");
+        scanner.nextLine(); // scanner eater line
+        //TODO add a scanner in here
 
         // Add to receipt:
         String sandwichDetails = size + "Size: " + bread + " Bread: " + toasted + "Toasted: " + meat + " Meat: " + extraMeat + "Extra Meat: " + cheese + " Cheese: " + extraCheese + "Extra Cheese: " + topping + "Topping: " + condiment + "Condiment: " + side + "Sides: ";
@@ -176,7 +198,9 @@ public class main {
         double price = getPrice(drinkSizeChoice, drinkChoices, drinkPrices);
         receiptItems.add("Drink: " + size + " - $" + price);
         totalPrice += price;
-        System.out.println("Added: " + size + " Drink ($" + price + ")"); } //TODO add a scanner in here and the eater code at the end
+        System.out.println("Added: " + size + " Drink ($" + price + ")"); }
+        scanner.nextLine(); // scanner eater line
+        //TODO add a scanner in here
 
     private static void addChips() {
         System.out.println("Ready for more? How about some chips to go with your sandwich?");
@@ -185,7 +209,9 @@ public class main {
         String chips = chooseOption("Chips", chipOptions, null);
         receiptItems.add("Chips: " + chips + " - $" + chipPrice);
         totalPrice += chipPrice;
-        System.out.println("Added: Chips - " + chips + " ($1.50)"); } //TODO add a scanner in here and the eater code at the end
+        System.out.println("Added: Chips - " + chips + " ($1.50)"); }
+        scanner.nextLine(); // scanner eater line
+        //TODO add a scanner in here and
 
     private static void checkout() {
         System.out.println("Let me take a look at what you've chosen and see what we have on this receipt: ");
@@ -204,10 +230,11 @@ public class main {
     //Receipt logic stuff:
     private static void saveReceipt() {
         String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss"));
-        String fileName = "soldiers-pyre-receipts/" + timestamp + ".txt";
+        String fileName = "C:/Users/Student/OneDrive - Year Up- BOS/Desktop/Year Up Courses/PluralSight/Year Up United Projects/LearnToCode_Capstones/SandwichShopCapstone" + timestamp + ".txt";
 
-        try (FileWriter writer = new FileWriter(fileName)) {
+        try (FileWriter writer = new FileWriter(fileName, true)) {
             writer.write("Let me take a look at what you've chosen and see what we have on this receipt: ");
+
             for (String item : receiptItems) {
                 writer.write(item + "\n"); }
 
@@ -215,6 +242,7 @@ public class main {
             writer.write("Soldier's Pyre Tavern and Deli: Eyes on victory, tummy on dinner.");
             writer.write("~This is my happy place~");
             System.out.println("Receipt saved under: " + fileName);
+
         } catch (IOException e) {
             System.out.println("Failed to save receipt: " + e.getMessage()); }
     }
